32eb8f53c4fad9eb42144729bb20a12a19b66a1a
ndungu22580-source <ndungu.22580@students.kyu.ac.ke>
2026-02-16 22:27:14 +0300

    Add files via upload

---
+++ UnitsOnOffer.java
@@ -0,0 +1,12 @@
+public class UnitsOnOffer{
+    public static void main (String[]args){
+        System.out.println("Units offered this semester:");
+        //Display each unit on a new line
+        System.out.println("Object Oriented Programming 2");
+        System.out.println("Object Oriented Analysis And Design");
+        System.out.println("Application Programming");
+        System.out.println("Cloud Computing");
+        System.out.println("Network Systems Administration");
+        System.out.println("Mobile Computing");
+    }
+} 
