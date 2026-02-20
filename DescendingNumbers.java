43575ffe858ed11159cc60e04e5e429582e85492
ndungu22580-source <ndungu.22580@students.kyu.ac.ke>
2026-02-20 18:43:15 +0300

    Add files via upload

---
+++ DescendingNumbers.java
@@ -0,0 +1,10 @@
+public class DescendingNumbers {
+    public static void main(String[] args) {
+        // A for loop that starts at 100, 
+        // continues as long as i is greater than or equal to 50,
+        // and decreases by 1 each time.
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+    }
+}
